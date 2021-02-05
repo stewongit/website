c1868c3a22e8610b8ea2653aaa1dee118cc2fae0
Stefan Ginther <stefan.ginther@rocksol-it.com>
2021-02-05 20:24:29 +0100

    page "Maschinensicherheit"

--- components/AppLoading.vue
+++ components/AppLoading.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="spinner-border spinner-border-sm" role="status">
-    <span class="visually-hidden">Loading...</span>
+  <div>
+    <div class="spinner-border spinner-border-sm text-primary" role="status">
+
+      <span class="sr-only">Loading...</span>
+    </div>
+    <br>
+    <div class="spinner-grow text-warning" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+    <div class="d-flex align-items-center">
+      <strong>Loading...</strong>
+      <div class="spinner-border spinner-border-sm ms-auto" role="status" aria-hidden="true"></div>
+    </div>
   </div>
 </template>
--- pages/maschinensicherheit.vue
+++ pages/maschinensicherheit.vue
@@ -2,19 +2,57 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <h1>CE-Kennzeichnung</h1>
+        <h1>Maschinensicherheit</h1>
         <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-
+          Konstrukteure und Anwendungstechniker für Maschinen und technische Anlagen sind laufend mit dem Thema Maschinensicherheit in ihrer Arbeit konfrontiert.
+          Aber auch Betreiber von Maschinen und Anlagen sollten aktuelle Richtlinien, Normen und Gesetze im Bereich der Maschinensicherheit stets im Auge haben, sei es für neue Maschinen, wie für gebrauchte.
         </p>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-      </div>
+
+        <h2 class="spacer">Gesetzliche Grundlagen der Maschinensicherheit</h2>
+        <p>
+          Bereits in der ersten Fassung der Maschinenrichtlinie von 1985, enthielt sie das „neue Konzept”, auch als „new approach to technical harmonization and standards”
+          oder Harmonisierungskonzept bekannt, zur Vereinheitlichung von Rechtsvorschriften in der EU. Sie hat damit das Produktrecht in Europa revolutioniert und einheitliche Standards
+          für alle Mitgliedsstaaten rund um das Inverkehrbringen technischer Produkte im Binnenmarkt der EU geschaffen.
+        </p>
+
+        <p>
+          Dabei sind zwei Grundsätze maßgeblich:
+          <ul>
+            <li>Die Anforderungen an Sicherheit und Gesundheitsschutz sollen in allen EU-Mitgliedsstaaten gleich lauten und für alle Hersteller und Vertreiber in diesen Staaten in gleichem Maße verbindlich gelten.</li>
+            <li>Wenn ein Produkt diese Anforderungen erfüllt, darf es in allen Mitgliedsstaaten frei gehandelt &dash; also in Verkehr gebracht &dash; werden.</li>
+          </ul>
+
+          Sowohl verbindliche Rechtsvorschriften der EU, als auch freiwillig anwendbare Europäische Normen (EN) definieren und spezifizieren die
+          grundlegenden Sicherheits- und Gesundheitsschutzanforderungen für Maschinen und Anlagen.
+        </p>
+        <p>
+          Für Betreiber und Hersteller von Maschinen gelten hier unterschiedliche Vorschriften in Bezug auf die Maschinensicherheit.
+          So richten sich <u>Betreiber</u> von Maschinen und Anlagen hauptsächlich nach der in Deutschland gültigen Betriebssicherheitsverordnung (BetrSichV) und nach der in
+          Österreich gültigen Arbeitsmittelverordnung (AM-VO) welche beide die Umsetzungen der europäischen Arbeitsmittelrichtlinie 89/655/EWG sind.
+          <u>Hersteller</u> hingegen stehen einer anderen Rechtslage gegenüber. Sie konzentrieren sich, neben anderen gültigen Normen, im Schwerpunkt auf die Maschinenrichtlinie 2006/42/EG.
+        </p>
+
+        <h2 class="spacer">Die Maschinenrichtlinie 2006/42/EG für Hersteller</h2>
+        <p>
+          Innerhalb der EU müssen alle technischen Erzeugnisse der Maschinenrichtlinie 2006/42/EG genügen. Verantwortlich hierfür ist der Hersteller sowie der Inverkehrbringer der Maschinen oder Anlagen.
+          Die Vorschriften gelten dabei nicht nur für vollständige Maschinen, sondern auch für unvollständige Maschinen.
+          Wichtiger Aspekt bei der Herstellung von sicheren Maschinen und Anlagen ist dabei die Risikobeurteilung (auch Gefahrenanalyse).
+        </p>
+
+        <h2 class="spacer">
+          Maschinensicherheit für Hersteller und Betreiber
+        </h2>
+        <p>
+          Zentraler Aspekt der Maschinensicherheit in Europa ist, dass sämtliche im Europäischen Wirtschaftsraum bereitgestellte Maschinen die Kriterien der Maschinensicherheit zu erfüllen haben.
+          Der Begriff der Bereitstellung beschreibt dabei Maschinen, die in Europa hergestellt oder auch wesentlich umgebaut (verändert) werden, wie auch Importe nach Europa, die anschließend vor Ort betrieben werden.
+          Hierbei werden also neue, als auch gebrauchte Maschinen und Anlagen betrachtet. Somit ist die Maschinenrichtlinie eine gesetzliche Vorschrift, die sowohl Hersteller als auch Betreiber von Maschinen zu berücksichtigen haben.
+          Die europäischen Richtlinien, wie beispielsweise die Maschinenrichtlinie oder die EMV-Richtlinie, führen Anforderungen für Maschinenhersteller und -betreiber auf, was die Modernisierung und Veränderung der Maschinen betrifft.
+          Seitens der Maschinenhersteller fällt dieser Aspekt unter das Stichwort der Produktsicherheit, während bei Maschinenbetreibern von Arbeitssicherheit die Rede ist.
+        </p>
+        <p>
+          Nicht nur in Europa spielt die Maschinensicherheit heute eine tragende Rolle, auch in vielen weiteren Ländern spielen Sicherheit von Mensch und Umwelt eine wachsende Rolle, auch ohne bislang bestehende gesetzliche Vorgaben.
+          Internationale Konzerne setzten zunehmend europäische Standards in punkto Maschinensicherheit in ihren Produktionsstätten weltweit um. Sichere Maschinen tragen dazu bei Unfälle zu vermeiden und heben so die Produktivität.
+        </p>      </div>
     </div>
 
   </div>
@@ -27,12 +65,12 @@ export default {
     htmlAttrs: {
       lang: 'de',
     },
-    title: 'CE-Kennzeichnung | Maschinen sicher',
+    title: 'Maschinensicherheit | Einführung',
     meta: [
       {
         hid: 'description',
         name: 'description',
-        content: 'Welches Verfahren muss für die Konformitätserklärung angewandt werden'
+        content: 'Grundlagen der Maschinensicherheit'
       }
     ],
   }
